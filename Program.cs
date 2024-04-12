1155fd1801d2e68d9eb1e593951a88bfc6a95d6b
Gaspar <gaspari.rodrigo@gmail.com>
2024-04-11 23:05:27 -0300

    Initial commit with comparer between ComponentModel validator and 'in house' validator

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+using using_reflection_in_a_validation_sample.ValidatorDoGaspar;
+
+Console.WriteLine("Building a new validator object using Reflaction");
+Console.WriteLine();
+
+
+//Objeto à ser validado:
+var novoBook = new LivroModel()
+{
+    //Titulo = "Código Limpo",
+    //Autor = "Robert C. Martin", 
+    Descricao = "Habilidades Práticas do Agile Software",
+    ISBN =  "978-85-7608-267-5", 
+    Idioma = "pt-BR",
+};
+
+
+Console.WriteLine("Usando validação nativa do Compoment Model"); 
+
+ValidationContext vc = new(novoBook);
+var validationResult = new List<ValidationResult>();
+
+if(!Validator.TryValidateObject(novoBook, vc, validationResult))
+    foreach (var item in validationResult)
+        Console.WriteLine($"  -> { item.ErrorMessage}");
+
+
+//Usando a nosso validador caseirinho :D
+var meuValidadorCaseiro = new ValidadorCaseiro(novoBook);
+
+meuValidadorCaseiro.ValidarObjeto();
+
+foreach (var item in meuValidadorCaseiro.Resultado.Erros)
+    Console.WriteLine($"  -> {item.Mensagem}" );
+
+
+
+
+public class LivroModel
+{
+    [Required]    // Componente Model.  
+    [Obrigatorio] // Validador "Caseiro"
+    public string? Titulo { get; set; }
+
+    [Required]
+    [Obrigatorio] // Validador "Caseiro"
+    public string? Autor { get; set; }
+
+    [MaxLength(250)]
+    [TamanhoMaximo(250)]
+    public string? Descricao { get; set; }
+    
+    [Required]
+    [Obrigatorio]
+    [StringLength(13, MinimumLength = 13)]
+    [TamanhoMaximo(13)]
+    public string? ISBN { get; set; }
+
+    [Required]
+    [Obrigatorio]
+    public string? Idioma { get;set; } 
+}
---
+++ ValidatorDoGaspar/ValidadorCaseiro.cs
@@ -0,0 +1,49 @@
+﻿namespace using_reflection_in_a_validation_sample.ValidatorDoGaspar
+{
+    public class ValidadorCaseiro(object? value)
+    {
+        private readonly object? _value = value;
+
+        public ValidadorCaseiroResultado Resultado { get; set; }
+
+        public void ValidarObjeto()
+        { 
+            if (_value != null) 
+            {
+                throw new ArgumentNullException();
+            }
+
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class ValidadorCaseiroResultado
+    { 
+        public ICollection<ValidadorCaseiroErro> Erros { get; set; }
+    }
+
+    public class ValidadorCaseiroErro
+    { 
+        public string? Mensagem { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class ObrigatorioAttribute : Attribute
+    {
+    
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public sealed class TamanhoMaximoAttribute : Attribute
+    {
+        int ? _tamanhoMaximo;
+
+        public TamanhoMaximoAttribute(int tamanho) 
+        { 
+            _tamanhoMaximo = tamanho;
+        }
+
+    }
+}
